336dc9fb80ab605e45c74d57600018ac0ca07cb8
Gustavo Teixeira <gustavo.teixeira@zoom.com.br>
2022-03-16 22:25:34 -0300

    applying prettier

--- src/lib/calculator.spec.js
+++ src/lib/calculator.spec.js
@@ -1,4 +1,4 @@
-const {sum} = require('./calculator');
+const { sum } = require('./calculator');
 
 it('should sum 2 an 2 and the result must be 4', () => {
   expect(sum(2, 2)).toBe(4);
@@ -9,41 +9,39 @@ it('should sum 2 an 2 even if one of them is a string and the result must be 4',
 });
 
 it('should throw an error if what is provided to the method cannot be summed', () => {
-  expect(()=>{
-    sum('a', '2')
+  expect(() => {
+    sum('a', '2');
   }).toThrowError();
 
-  expect(()=>{
-    sum([2, 2])
+  expect(() => {
+    sum([2, 2]);
   }).toThrowError();
 
-  expect(()=>{
-    sum([2, 2], 2)
+  expect(() => {
+    sum([2, 2], 2);
   }).toThrowError();
 
-  expect(()=>{
-    sum([2, 2], [2, 2])
+  expect(() => {
+    sum([2, 2], [2, 2]);
   }).toThrowError();
 
-  expect(()=>{
-    sum(true, 2)
+  expect(() => {
+    sum(true, 2);
   }).toThrowError();
 
-  expect(()=>{
-    sum({})
+  expect(() => {
+    sum({});
   }).toThrowError();
 
-  expect(()=>{
-    sum({number:2}, 2)
+  expect(() => {
+    sum({ number: 2 }, 2);
   }).toThrowError();
 
-  expect(()=>{
-    sum(2)
+  expect(() => {
+    sum(2);
   }).toThrowError();
 
-
-
-  expect(()=>{
-    sum()
+  expect(() => {
+    sum();
   }).toThrowError();
 });
