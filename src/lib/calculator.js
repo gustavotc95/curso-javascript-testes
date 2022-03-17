9ae33af1e6e321515052c1d7615aefff8c1db0cb
Gustavo Teixeira <gustavo.teixeira@zoom.com.br>
2022-03-16 22:21:54 -0300

    refactoring functions

--- src/lib/calculator.js
+++ src/lib/calculator.js
@@ -1,15 +1,14 @@
 module.exports.sum = (num1, num2) => {
-  if(num1[1] || num2[1]){
-    if(Number.isNaN(intNum1) || Number.isNaN(intNum2)){
-      throw new Error("Please check your input, they must be a number")
-    }
-  }
-
   const intNum1 = parseInt(num1, 10);
   const intNum2 = parseInt(num2, 10);
 
-  if(Number.isNaN(intNum1) || Number.isNaN(intNum2)){
-    throw new Error("Please check your input, they must be a number")
+  if (
+    Number.isNaN(intNum1) ||
+    Number.isNaN(intNum2) ||
+    Array.isArray(num1) ||
+    Array.isArray(num2)
+  ) {
+    throw new Error('Please check your input, they must be a number');
   }
 
   return intNum1 + intNum2;
